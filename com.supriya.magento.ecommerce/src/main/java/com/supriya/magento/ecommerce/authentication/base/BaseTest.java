196fa8c2bc6834ce6f736f3d27fdac29e3acc3ed
supriya <supriya@ameyas-mbp.fritz.box>
2025-06-18 23:28:59 +0200

    feat: add PropertyUtility class to read config values from properties file

--- com.supriya.magento.ecommerce/src/main/java/com/supriya/magento/ecommerce/authentication/base/BaseTest.java
+++ com.supriya.magento.ecommerce/src/main/java/com/supriya/magento/ecommerce/authentication/base/BaseTest.java
@@ -1,5 +1,15 @@
 package com.supriya.magento.ecommerce.authentication.base;
 
+import org.openqa.selenium.WebDriver;
+
 public class BaseTest {
 
+	
+	public static WebDriver driver = null;
+	
+	public void initialization() {
+		
+	}
+	
+	
 }
--- com.supriya.magento.ecommerce/src/main/java/com/supriya/magento/ecommerce/authentication/utilities/PropertyUtility.java
+++ com.supriya.magento.ecommerce/src/main/java/com/supriya/magento/ecommerce/authentication/utilities/PropertyUtility.java
@@ -1,5 +1,21 @@
 package com.supriya.magento.ecommerce.authentication.utilities;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
 public class PropertyUtility {
+	
+	
+	
+	public static String readProperty(String key) throws Exception {
+		
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "src/main/resources/properties/config.properties"); 	
+
+		prop.load(fis);
+		return prop.getProperty(key);
+		
+	}
 
 }
