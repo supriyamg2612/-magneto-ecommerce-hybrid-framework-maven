eb5c0a68e3c3d72d7e3c881b6d3edcfd5b77676a
supriya <supriya@ameyas-mbp.fritz.box>
2025-06-18 23:37:30 +0200

    chore: add WebDriver initialization logic to BaseTest class

--- com.supriya.magento.ecommerce/src/main/java/com/supriya/magento/ecommerce/authentication/base/BaseTest.java
+++ com.supriya.magento.ecommerce/src/main/java/com/supriya/magento/ecommerce/authentication/base/BaseTest.java
@@ -1,15 +1,36 @@
 package com.supriya.magento.ecommerce.authentication.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.supriya.magento.ecommerce.authentication.utilities.PropertyUtility;
 
 public class BaseTest {
 
 	
 	public static WebDriver driver = null;
 	
-	public void initialization() {
+	public void initialization() throws Exception {
+		String browserName = PropertyUtility.readProperty("browser"); //reading browser name
+		if(browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+			driver = new ChromeDriver();
+		}
+
+		if(browserName.equalsIgnoreCase("firefox")) {
+
+		System.setProperty("webdriver.gecko.driver", "driver/geckodriver");
+
+		driver = new FirefoxDriver();
+
+		}
+		driver.manage().window().maximize();
+
+		driver.get(PropertyUtility.readProperty("url"));
+		}
 		
-	}
+	
 	
 	
 }
